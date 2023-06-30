705f70b59efd864c4b2588e6ba5cf6940d055efb
arlestaboada <31904068+arlestaboada@users.noreply.github.com>
2023-06-30 11:12:00 -0500

    Configurar el router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navigation from "./layouts/Navigation";
+import { Route, BrowserRouter as Router,Routes} from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Posts from "./pages/Posts";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    <Navigation></Navigation>
+    <Router>
+      <div>
+        <Navigation>
+
+        </Navigation>
+        <Container>
+          <Routes>
+            <Route exact path="/" Component={Posts}></Route>
+            <Route exact path="/signin" Component={SignIn}></Route>
+            
+
+          </Routes>
+          
+        </Container>
+
+      </div>
+    </Router>
+   
   );
 }
 
--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Nav,NavDropdown,Navbar } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 export default function Navigation() {
   return (
    <Navbar bg="dark" variant="dark" expand="lg">
-    <Navbar.Brand>React Java</Navbar.Brand>
+    <Navbar.Brand as={NavLink} to="/">React Java</Navbar.Brand>
     <Navbar.Toggle aria-controls="main-menu"></Navbar.Toggle>
     <Navbar.Collapse id="main-menu">
         <Nav className="me-auto">
@@ -12,7 +13,8 @@ export default function Navigation() {
         </Nav>
        
         <Nav > 
-            <Nav.Link>Crear cuenta</Nav.Link>
+              <Nav.Link>Crear cuenta</Nav.Link>
+              <Nav.Link as={NavLink} to="/signin">Iniciar sesion</Nav.Link>
               <NavDropdown title="arles taboada" id="menu-dropdown">
                 <NavDropdown.Item>Posts</NavDropdown.Item>
                 <NavDropdown.Item>Cerrar sesion</NavDropdown.Item>
