098ac8ae83699d4fd07b0d36679744a549b695f0
arlestaboada <31904068+arlestaboada@users.noreply.github.com>
2023-07-02 22:12:23 -0500

    Configurar react redux
    Configurar react redux

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import Navigation from "./layouts/Navigation";
-import { Route, BrowserRouter as Router,Routes} from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Posts from "./pages/Posts";
-import SignIn from "./pages/SignIn";
+import Navigation from "./layouts/Navigation"
+import { Route, BrowserRouter as Router,Routes} 
+from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Posts from "./pages/Posts"
+import SignIn from "./pages/SignIn"
+import store from "./store"
+import { Provider } from "react-redux"
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navigation>
+    <Provider store={store}>
+        <Router>
+          <div>
+            <Navigation>
 
-        </Navigation>
-        <Container>
-          <Routes>
-            <Route exact path="/" Component={Posts}></Route>
-            <Route exact path="/signin" Component={SignIn}></Route>
-            
+            </Navigation>
+            <Container>
+              <Routes>
+                <Route exact path="/" Component={Posts}></Route>
+                <Route exact path="/signin" Component={SignIn}></Route>
+                
 
-          </Routes>
-          
-        </Container>
+              </Routes>
+              
+            </Container>
 
-      </div>
-    </Router>
+          </div>
+      </Router>
+
+    </Provider>
+    
    
   );
 }
---
+++ src/reducers/authReducer.js
@@ -0,0 +1,21 @@
+import { SET_CURRENT_USER } from "../actions/types"
+
+const initialState={loggedIn:false,user:{}}
+
+const authReducer = function(state=initialState,action) {
+  const {type,payload}=action
+  switch(type){
+
+    case SET_CURRENT_USER:
+        return {
+            ...state,
+            loggedIn:payload.loggedIn,
+            user:payload.user
+        }
+    default:
+        return state
+
+
+  }
+}
+export default authReducer;
