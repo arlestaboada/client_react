f50d98afb5bbb193df77f741c9cf9a2ec611f7f2
arlestaboada <31904068+arlestaboada@users.noreply.github.com>
2023-07-05 15:21:05 -0500

    Validar formulario de inicio de sesión

---
+++ src/components/forms/SignInForm.js
@@ -0,0 +1,62 @@
+import React,{useState} from 'react'
+import {Form, Button}   from 'react-bootstrap'
+
+export default function SignInForm({errors,onSubmitCallback}) {
+    const[email,setEmail]=useState("")
+   const [password, setPassword] = useState("")
+
+   const submitForm=(e)=>{
+    e.preventDefault()
+    onSubmitCallback({email,password})
+
+   }
+
+
+  return (
+    
+    <Form onSubmit={submitForm}>
+        <Form.Group control="email">
+            <Form.Label>Correo electrónico</Form.Label>
+            <Form.Control
+                type="email"
+                value={email}
+                onChange={e=>setEmail(e.target.value)}
+                placeholder='Correo electrónico'
+                isInvalid={errors.email}
+            />
+            <Form.Control.Feedback type="invalid">
+                {  console.log( errors.email)}
+                {
+                  
+                errors.email
+                
+                }
+
+            </Form.Control.Feedback>
+
+
+           
+
+        </Form.Group>
+
+         <Form.Group control="password">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control
+                type="password"
+                value={password}
+                onChange={e=>setPassword(e.target.value)}
+                placeholder='Contraseña'
+                isInvalid={errors.password}
+            />
+            <Form.Control.Feedback type="invalid">
+                {errors.password}
+
+            </Form.Control.Feedback>
+
+        </Form.Group>
+
+        <Button variant='primary' type="submit">Iniciar sesión</Button>
+
+    </Form>
+  )
+}
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Container,Row,Col,Card } from 'react-bootstrap'
+import SignInForm from '../components/forms/SignInForm'
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+import validator from 'validator'
+import { isObjEmpty } from '../helpers/helpers'
 
 export default function SignIn() {
+
+  const [errors, setErrors] = useState({})
+
+  const dispatch=useDispatch()
+  const login =({email,password})=>{
+    const errors={}
+    setErrors(errors)
+    if(!validator.isEmail(email)){
+      errors.email="El correo electrónico es inválido."
+
+    }
+    if(validator.isEmpty(password)){
+      errors.password="La contraseña no puede estar vacia."
+
+    }
+
+    if(!isObjEmpty(errors)){
+     
+      setErrors(errors)
+      return
+
+    }
+  }
+ 
   return (
-    <div>SignIn</div>
+   <Container>
+      <Row>
+        <Col sm="12" md={{span:8, offset:2}} lg={{span:6,offset:3}}>
+          <Card body>
+            <h3>Iniciar sesión</h3>
+            <SignInForm errors={errors} onSubmitCallback={login}></SignInForm>
+            <div className='mt-4'>
+              <Link to={"/signup"}>¿No tienes una cuenta? Registrate aquí</Link>
+
+            </div>
+
+          </Card>
+        
+        </Col>
+      </Row>
+   </Container>
   )
 }
