7cfda1c57d9f8ba0bf8a2401427b036105afd156
arlestaboada <31904068+arlestaboada@users.noreply.github.com>
2023-08-02 23:42:40 -0500

    Creando vista detalles del post parte 2 eInstalar SystaxHighlighter

--- src/pages/PostDetails.js
+++ src/pages/PostDetails.js
@@ -2,6 +2,10 @@ import React,{ useState,useEffect } from 'react'
 import { useParams,useNavigate } from 'react-router-dom'
 import { POSTS_DETAILS_ENDPOINT } from '../helpers/endpoints'
 import axios from 'axios'
+import moment from 'moment/moment'
+import { Button,Card } from 'react-bootstrap'
+import {  Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+
 
 
 export default function PostDetails() {
@@ -24,6 +28,41 @@ export default function PostDetails() {
       },[])
 
   return (
-    <div>PostDetails</div>
+    <div className='pb-4'>
+          {
+           post &&(
+           <React.Fragment>
+              <Card className='mb-4'> 
+                <Card.Header>
+                  <h1>{post.title}</h1>
+                  <p>Creado por {post.user.firstName},
+                    {moment(post.createdAt).fromNow()}
+                  </p>
+
+                </Card.Header>
+               
+              </Card>
+    
+              <Card>
+                <Card.Header>
+                  <Button variant='primary' className='me-2' size='sm' onClick={()=>{}}>
+                    Descargar
+                  </Button>
+                  <Button variant='primary'    size='sm' onClick={()=>{}}>
+                    Copiar al portapapeles
+                  </Button>
+                </Card.Header>
+                    <Card.Body>
+                      <SyntaxHighlighter showLineNumbers={true}>
+                        {post.content}
+                      </SyntaxHighlighter>
+                      
+                    </Card.Body>
+              </Card>
+            </React.Fragment>)
+            
+          }
+    </div>
+    
   )
 }
