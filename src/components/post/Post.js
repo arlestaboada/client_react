96120a071e163be98f6f7725ff297dab6dbcf929
arlestaboada <31904068+arlestaboada@users.noreply.github.com>
2023-08-05 15:45:20 -0500

    Añadir botones a controles

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Badge, Card } from 'react-bootstrap'
+import { Badge,Button, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import moment from "moment"
 
@@ -8,7 +8,7 @@ import { exposures } from '../../helpers/exposures'
 export default function Post({post, renderControls}) {
   return (
     <Card className='mb-4'>
-      {renderControls && <Card.Header>
+      {renderControls && <Card.Header className='d-flex justify-content-between'>
         <div>
           <Badge
             className='me-2 bg-secondary' >
@@ -19,6 +19,11 @@ export default function Post({post, renderControls}) {
             Expiró
           </Badge>}
         </div>
+        <div>
+          <Button variant="primary" size="sm" className='me-2'>Editar</Button>
+          <Button variant="danger" size="sm">Eliminar</Button>
+        </div>
+
       </Card.Header> }
       
       <Card.Body>
